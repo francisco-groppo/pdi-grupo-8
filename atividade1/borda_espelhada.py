dc0a18ea6067a96db519bb66b8ea9060a8f60717
Francisco Groppo <groppofilho@yahoo.com.br>
2021-05-28 22:44:54 -0300

    Correção em borda_espelhada

--- atividade1/borda_espelhada.py
+++ atividade1/borda_espelhada.py
@@ -4,65 +4,70 @@
 # In[20]:
 
 
-
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
+
+
+def mirror_borda(original, borda):
+    rows, columns = original.shape
+
+    changed = np.zeros((rows+2*borda, columns+2*borda))
+
+    # corners
+    # top - left
+    changed[0:borda, 0: borda] = np.flipud(
+        np.fliplr(original[1:borda+1, 1: borda+1]))
+    # bottom - left
+    changed[rows + borda:,
+            0: borda] = np.flipud(np.fliplr(original[rows-borda-1: -1,  1: borda+1]))
+    # top - right
+    changed[0: borda, columns +
+            borda:] = np.flipud(np.fliplr(original[1:borda+1, columns-borda-1: -1]))
+    # bottom - right
+    changed[rows + borda:, columns + borda:] = np.flipud(
+        np.fliplr(original[rows-borda-1: -1, columns-borda-1: -1]))
+
+    # original
+    changed[borda: rows+borda, borda: columns + borda] = original
+
+    # left
+    changed[borda: rows+borda,
+            0: borda] = np.fliplr(original[0:rows, 1:borda+1])
+
+    # right
+    changed[borda: rows+borda, columns +
+            borda:] = np.fliplr(original[0:rows, columns-borda-1: -1])
+
+    # top
+    changed[0: borda, borda: borda +
+            columns] = np.flipud(original[1:borda+1, 0:columns])
+
+    # bottom
+    changed[rows+borda:, borda: borda +
+            columns] = np.flipud(original[rows-borda-1:-1, 0:columns])
+
+    return changed
+
+
+def visualize(original, changed):
+    plt.matshow(original)
+    plt.matshow(changed)
+    plt.show()
 
 
 img = cv2.imread('cameraman.tiff')
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 
-# modificando o tamanho da imagem para uma matriz 3x3
-img = cv2.resize(img, (10, 9))
+# modificando o tamanho da imagem para uma matriz nxm
+# img = cv2.resize(img, (3, 9))
 
-original = np.array([
-    [2, 3, 1, 4, 5],
-    [1, 5, 3, 7, 4],
-    [2, 9, 2, 0, 0],
-    [8, 7, 2, 4, 3]
-   
-])
+borda = 2
 
-borda=2
-
-
-rows, columns = original.shape
-
-changed = np.zeros((rows+2*borda, columns+2*borda))
-
-
-# corners 
-#top -left
-changed[0:borda, 0: borda] = np.flipud(np.fliplr(original[1:borda+1, 1: borda+1]))
-#botton - left
-changed[rows + borda:, 0: borda] = np.flipud(np.fliplr(original[rows-borda-1: -1,  1: borda+1]))
-#top - right
-changed[0: borda, columns +borda:] = np.flipud(np.fliplr(original[1:borda+1, columns-borda-1: -1]))
-#botton - right
-changed[rows + borda:, columns +borda:] = np.flipud(np.fliplr(original[rows-borda-1: -1, columns-borda-1: -1]))
-
-# original
-changed[borda: rows+borda, borda: columns + borda]= original
-
-# left
-changed[borda: rows+borda, 0: borda]  = np.fliplr(original[0:rows, 1:borda+1])
-
-# right
-changed[borda: rows+borda, rows+borda:] = np.fliplr(original[0:rows, columns-borda-1: -1])
-
-# top
-changed[0: borda, borda: borda + columns] = np.flipud(original[1:borda+1, 0:columns])
-
-# bottom
-changed[columns+borda:, borda : borda + columns] = np.flipud(original[rows-borda-1:-1, 0:columns])
-print(original)
-print(changed)
-
+if __name__ == "__main__":
+    changed = mirror_borda(img, borda)
+    print(changed)
+    visualize(img, changed)
 
 # In[ ]:
-
-
-
-
--- atividade1/vizinho_mais_proximo.py
+++ atividade1/vizinho_mais_proximo.py
@@ -15,66 +15,66 @@ import matplotlib.pyplot as plt
 import cv2
 
 
+def nearest_neighbor_fill(original, borda):
+    rows, columns = original.shape
+
+    changed = np.zeros((rows+2*borda, columns+2*borda))
+
+    # corners
+    # top - left
+    changed[0:borda + 1, 0: borda + 1] = original[0, 0]
+    # bottom - left
+    changed[rows + borda - 1:, 0: borda + 1] = original[rows-1, 0]
+    # top - right
+    changed[0:borda + 1, columns + borda - 1:] = original[0, columns-1]
+    # bottom - right
+    changed[rows + borda - 1:, columns +
+            borda - 1:] = original[rows-1, columns-1]
+
+    # original
+    changed[borda: rows + borda, borda: columns + borda] = original
+
+    # left
+    changed[borda + 1: rows + borda - 1,
+            0: borda] = np.array([original[1:rows-1, 0]]).T
+
+    # right
+    changed[borda + 1: rows + borda - 1, columns +
+            borda:] = np.array([original[1:rows-1, -1]]).T
+
+    # top
+    changed[0: borda, borda + 1: columns +
+            borda - 1] = original[0, 1:columns-1]
+
+    # bottom
+    changed[rows + borda:, borda + 1: columns +
+            borda - 1] = original[-1, 1:columns-1]
+
+    return changed
+
+
+def visualize(original, changed):
+    plt.matshow(original)
+    plt.matshow(changed)
+    plt.show()
+
+
 img = cv2.imread('cameraman.tiff')
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-
-# modificando o tamanho da imagem para uma matriz 3x3
-img = cv2.resize(img, (10, 9))
-
-
-
-########### Francisco ##############
-
-print('----------------')
+# modificando o tamanho da imagem para uma matriz nxm
+# img = cv2.resize(img, (48, 48))
 
 original = img
 
-rows, columns = original.shape
-#print(rows)
-#print(columns)
+borda = 2
 
-changed = np.zeros((rows+4, columns+4))
-#print(original[rows-1, 0])
-
-# corners
-changed[0:3, 0: 3] = original[0, 0]
-changed[rows+1:, 0: 3] = original[rows-1, 0]
-changed[0:3, columns+1:] = original[0, columns-1]
-changed[rows +1:, columns+1:] = original[rows-1, columns-1]
-
-# original
-changed[2: rows+2, 2: columns +2] = original
-
-# left
-changed[3: rows+1, 0: 2] = np.array([original[1:rows-1, 0]]).T
-
-# right
-changed[3: rows+1, columns + 2:] = np.array([original[1:rows-1, -1]]).T
-
-# top
-changed[0: 2, 3: columns +1] = original[0, 1:columns-1]
-
-
-# bottom
-changed[rows +2:, 3: columns +1] = original[-1, 1:columns-1]
-
-#plt.matshow(original)
-#plt.matshow(changed)
-#plt.show()
-
-print(f'Original:\n {img}\n')
-
-
-print(f'Com pad nos cantos:\n {changed.astype(int)}\n')
-
-#print(changed.astype(int))
+if __name__ == "__main__":
+    changed = nearest_neighbor_fill(img, borda)
+    print(changed)
+    visualize(img, changed)
 
 # In[ ]:
 
 
 # In[ ]:
-
-
-
-
